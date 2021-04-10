4cab82f4b12449d10a377d40caa3725ea8d87a3a
Steven Platt <31355889+stevenplatt@users.noreply.github.com>
2021-04-10 22:31:27 +0200

    Update vm_deploy.sh
    Updated to include vm deploy code (commented out)

--- deploy_scripts/vm_deploy.sh
+++ deploy_scripts/vm_deploy.sh
@@ -10,6 +10,17 @@ download_images(){
     cd
 }
 
+# deploy VMs
+# https://forum.proxmox.com/threads/vm-create-edit-delete-with-api.37743/
+# https://gist.github.com/dragolabs/f391bdda050480871ddd129aa6080ac2
+
+
+#  pvesh create /nodes/proxmox/qemu -name pop-os-tester02 -vmid 121 \
+#             -scsi0 local-lvm:32 \
+#             -memory 8192 -cpu host -socket 1 -cores 4 \
+#             -net0 virtio,bridge=vmbr0 \
+#             -ide2 local:iso/pop-os_20.10_amd64_intel_14.iso,media=cdrom
+
 ######### Welcome message #########
 printf "\n\nDownloading operating system images...\n"
 
