07f4f90425b3153771b92eae9702d041ef8a2744
Steven Platt <mr.platt@gmail.com>
2021-08-20 20:09:02 -0700

    Updated terraform deployment to include load bal.

--- kubernetes_deploy.tf
+++ kubernetes_deploy.tf
@@ -13,10 +13,49 @@ provider "digitalocean" {
     token = var.do_token
 }
 
+
+# Deploy Load Balancer Node
+resource "digitalocean_droplet" "load_balancer" {
+  name   = "k8s-lb-droplet"
+  size   = "s-1vcpu-1gb"
+  image  = "ubuntu-20-04-x64"
+  region = "sfo3"
+}
+
+resource "digitalocean_loadbalancer" "k8s_lb_resource" {
+  name   = "k8s-loadbalancer"
+  region = "sfo3"
+  algorithm = "least_connections"
+
+  forwarding_rule {
+    entry_port     = 80
+    entry_protocol = "http"
+
+    target_port     = 80
+    target_protocol = "http"
+  }
+
+  sticky_sessions {
+    type  = "cookies"
+    cookie_name = "telecomsteve"
+    cookie_ttl_seconds = 3600
+  }
+
+  healthcheck {
+    port     = 22
+    protocol = "tcp"
+  }
+
+  droplet_ids = [digitalocean_droplet.load_balancer.id]
+}
+
+
+
 data "digitalocean_kubernetes_versions" "homelab" {
   version_prefix = "1.21." # allow auto upgrades for patch releases of this base version
 }
 
+# Deploy K8s cluster
 resource "digitalocean_kubernetes_cluster" "homelab" {
   name    = "homelab"
   region  = "sfo3"
@@ -37,3 +76,4 @@ resource "digitalocean_kubernetes_cluster" "homelab" {
     max_nodes  = 2
   }
 }
+
