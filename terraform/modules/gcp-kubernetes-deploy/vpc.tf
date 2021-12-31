f15968dc3c5506ca7575d7c40b95f82e0abafb98
Steven Platt <31355889+stevenplatt@users.noreply.github.com>
2021-12-30 22:04:33 -0800

    Update vpc.tf
    added terraform gke firewall configuration

--- terraform/modules/gcp-kubernetes-deploy/vpc.tf
+++ terraform/modules/gcp-kubernetes-deploy/vpc.tf
@@ -11,3 +11,31 @@ resource "google_compute_subnetwork" "subnet" {
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
 }
+
+################################################################
+# Assigning firewall rules to the network IP space
+################################################################
+
+# source = https://github.com/MinaProtocol/mina/blob/develop/automation/terraform/modules/google-cloud/vpc-network/main.tf
+resource "google_compute_firewall" "homelab_ingress" {
+  name    = "${var.project_id}-homelab-ingress"
+  network = google_compute_network.vpc.name
+
+  source_ranges = ["0.0.0.0/0"]
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["10000-11000"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports    = ["10000-11000"]
+  }
+
+  source_tags = ["homelab-ports"]
+}
